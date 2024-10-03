c4fb3a76914e7f3480394e1cfdc317ad08db631e
Vari Balazs <vari.balazs01@gmail.com>
2024-10-03 12:12:18 +0200

    creating nvim submodule

--- nvim/lua/bulazs/init.lua
+++
@@ -1,3 +0,0 @@
-require("bulazs.remap")
-require("bulazs.set")
-require("bulazs.lazy_init")
--- nvim/lua/bulazs/lazy/undotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "mbbill/undotree",
-
-    config = function()
-        vim.g.undotree_WindowLayout = 2
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end,
-}
